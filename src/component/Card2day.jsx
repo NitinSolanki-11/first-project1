bdc8e779342d5d0f301704d14a34c32e21e26a06
Nitin Solanki <nitin5115solanki@gmail.com>
2024-12-10 10:42:02 +0530

    Add files via upload

---
+++ src/component/Card2day.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card2day = () => {
+  return (
+    <>
+     <div className='box1'>
+            <div className="imgg"> <img src="public/tim-marshall-Y8Cs1hhBoXA-unsplash.jpg" alt="" /></div>
+            <div className="para">
+              <h3>4 Days Ago</h3>
+              <span><h1>POST ONE</h1></span>
+              <span><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam omnis enim esse iste nobis culpa, aliquam reprehenderit amet totam, modi maiores maxime! Aut, nulla? Iusto eveniet expedita consequatur corporis aperiam.</p></span>
+              </div>
+            <div className="count"></div>
+     </div>
+    </>
+  )
+}
+
+export default Card2day
